ced4abc7cf8518c8795b29ae511408711d925a12
Santanu Paik <git@iocle.com>
2024-10-03 19:55:22 +0530

    Add nginx setup script

---
+++ server/nginx/https.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Check for root user
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root !!"
+  exit
+fi
+
+# Variables
+DOMAIN=DOMAINSHOULDBECHANGED
+EMAIL=EMAILSHOULDBECHANGED
+
+# Get user confirmation for stored variables
+printf "\nStored credentials:\n"
+printf "\nDomain: %s\n" "$DOMAIN"
+printf "Email: %s\n\n" "$EMAIL"
+
+read -p "Are the Stored Credentials Correct ? (y/n): " -r ans
+case $ans in
+    [Yy]* ) printf "Great ! Proceeding with installation..."
+            ;;
+            
+    [Nn]* ) echo
+            read -p "Enter Your Email: " -r EMAIL
+	    read -p "Enter Your Domain: " -r DOMAIN
+	    ;;
+	    
+    * )     echo "Please re-run the script and answer in (y)es or (n)o."
+    	    exit 0
+    	    ;;
+esac
+
+# Intro Message
+printf "\nThis script will setup https with Nginx \n"
+
+# Set the Present Working Directory
+DIR=$(pwd)
+
+# Install acme.sh client
+printf "\nInstalling acme.sh client...\n"
+curl https://get.acme.sh | sh -s email="$EMAIL"
+
+# Creating required directories
+printf "\nCreating the required Directories...\n"
+mkdir -pv /var/log/nginx/"$DOMAIN"
+mkdir -pv /etc/nginx/ssl/"$DOMAIN"
+
+# Generating dhparams for ssl
+printf "\nGenerating dhparams for SSL...\n"
+cd /etc/nginx/ssl/"$DOMAIN" || exit
+openssl dhparam -out dhparams.pem -dsaparam 4096
+cd "$DIR" || exit
+
+# Issue an ssl/tls Certificate from LetsEncrypt
+printf "\nIssuing TLS Certificate...\n"
+bash /root/.acme.sh/acme.sh \
+--issue \
+-d "$DOMAIN" \
+-d www."$DOMAIN" \
+-w /var/www/"$DOMAIN" \
+-k 4096 \
+--server letsencrypt
+
+# Copy https config to required place
+printf "\nCopying https config...\n"
+< nginx-https.conf tee /etc/nginx/sites-available/"$DOMAIN" >/dev/null
+
+# Install the ssl/tls certificate in Nginx & reload it
+printf "\nInstalling the TLS Certificate...\n"
+bash /root/.acme.sh/acme.sh \
+--install-cert -d "$DOMAIN" \
+--cert-file /etc/nginx/ssl/"$DOMAIN"/"$DOMAIN".cert \
+--key-file /etc/nginx/ssl/"$DOMAIN"/"$DOMAIN".key \
+--fullchain-file /etc/nginx/ssl/"$DOMAIN"/"$DOMAIN".fullchain \
+--reloadcmd "systemctl restart nginx.service"
---
+++ server/nginx/setup.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Check & grant sudo priviledges
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
+# Intro Message
+printf "\nThis script will setup Nginx & UFW \nSet your domain name (example.com) WITHOUT any Sub-domain (www.)\n\n"
+
+# Get Domain Name from User
+read -p "Enter Your Domain Name: " -r DOMAIN
+read -p "Enter Your Email ID: " -r EMAIL
+
+# Update & Install required Packages
+printf "\nUpdating & Installing Required Packages...\n"
+sudo apt-get update
+sudo apt install -y git bc wget vim htop openssl curl socat rsync ufw nginx
+
+# Start required Services
+printf "\nStarting Systemd Services...\n"
+sudo systemctl enable --now nginx
+sudo systemctl enable --now ssh
+sudo systemctl enable --now rsync
+sudo systemctl enable --now ufw
+
+# Setup UFW to allow ssh & Nginx
+printf "\nSetting up UFW...\n"
+sudo ufw allow ssh
+sudo ufw allow 'Nginx Full'
+yes | sudo ufw enable
+
+# Create required directories
+printf "\nCreating Required Directories...\n"
+sudo mkdir -p /var/www/"$DOMAIN"
+sudo mkdir -p /etc/nginx/includes
+sudo mkdir -p /var/www/"$DOMAIN"/.well-known/acme-challenge/
+sudo mkdir -p /root/Server
+
+# Changing config files according to user input
+sed -i "s/DOMAIN/$DOMAIN/g" http.conf
+sed -i "s/DOMAIN/$DOMAIN/g" https.conf
+sed -i "s/DOMAIN/$DOMAIN/g" webroot.conf
+sed -i "s/DOMAINSHOULDBECHANGED/$DOMAIN/g" https.sh
+sed -i "s/EMAILSHOULDBECHANGED/$EMAIL/g" https.sh
+
+# Move https related files to root
+printf "\nMoving https related files to root...\n"
+sudo mv https.sh /root/Server/
+sudo mv https.conf /root/Server/
+
+# Set appropriate directory permissions
+printf "\nSetting Necessary Folder Permissions...\n"
+sudo chown -R "$USER":"$USER" /var/www/"$DOMAIN"
+sudo chmod -R 755 /var/www/"$DOMAIN"
+sudo chown -R www-data:www-data /var/www/"$DOMAIN"/.well-known/acme-challenge/
+sudo chmod -R 0555 /var/www/"$DOMAIN"/.well-known/acme-challenge/
+
+# Copy required configuration to their appropriate places
+printf "\nCopying required config files...\n"
+mv nginx-website/* /var/www/"$DOMAIN"/
+< webroot.conf sudo tee /etc/nginx/includes/letsencrypt-webroot >/dev/null
+< http.conf sudo tee /etc/nginx/sites-available/"$DOMAIN" >/dev/null
+
+# Symlink nginx config
+printf "\nFinalizing Nginx Server Setup...\n"
+sudo ln -s /etc/nginx/sites-available/"$DOMAIN" /etc/nginx/sites-enabled/
+
+# Set nginx hash bucket size
+sudo sed -i 's/# server_names_hash_bucket_size/server_names_hash_bucket_size/g' /etc/nginx/nginx.conf
+
+# Check nginx config & restart it
+printf "\nVerifying Nginx config...\n"
+sudo nginx -t
+printf "\nRestarting Nginx...\n"
+sudo systemctl restart nginx
+
+# Inform the user about running the https script
+printf "\nChange to root user and run https-setup.sh stored inside the Server folder to get https support and complete the Setup.\n"
